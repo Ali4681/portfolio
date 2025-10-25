61592c2846e4fbb755cbb7851310b553a1d244f0
ali4681 <hasan.zarangy@gmail.com>
2025-10-26 00:59:28 +0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,23 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Professional Portfolio</title>
+        <title>Ali Al-bayati</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Open Graph Image (for social media) */}
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:title" content="Ali Al-bayati" />
+        <meta property="og:description" content="Your description here" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/twitter-image.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
